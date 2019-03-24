f1f8d8340e7612ccb4fc7335fc57ee4e12cf7576
Julian Vila <vilaj46@gmail.com>
2019-03-24 18:21:38 -0400

    hey

--- build/index.js
+++ build/index.js
@@ -33,7 +33,6 @@ app.get('/*', function (req, res) {
 app.post('/contact', function (req, res) {
   var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
   var information = Object.keys(req.body).length > 0 ? JSON.parse(Object.keys(req.body)[0]) : null;
-  res.send(information);
 
   if (information) {
     var transporter = _nodemailer.default.createTransport({
